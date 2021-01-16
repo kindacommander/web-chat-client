e0515a5b04bbc502ce65641283f7e70b13067f8d
kindacommander <artem.sandyuk@gmail.com>
2021-01-17 00:24:51 +0200

    Project structured.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:web_chat_client/main.dart';
+import 'package:web_chat_client/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
